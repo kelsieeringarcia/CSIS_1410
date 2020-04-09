4f3fd72aebe8d31fb80aa51cfaf31aee5c59ac19
kelsieeringarcia <36800046+kelsieeringarcia@users.noreply.github.com>
2020-04-08 19:27:36 -0600

    methods created

---
+++ FitnessCalculator/src/fitnessCalculator/Calculations.java
@@ -0,0 +1,52 @@
+package fitnessCalculator;
+
+public class Calculations {
+	/**
+	 * This will calculate the users Body mass index
+	 * @param height
+	 * @param weight
+	 * @param e
+	 * @return
+	 */
+	public int bmiCalculator(int height, int weight, UnitType e) {
+		return 0;
+	}
+	/**
+	 * This will calculate the users Basal metabolic rate
+	 * @param height
+	 * @param weight
+	 * @param age
+	 * @param gender 0 = male & 1 = female
+	 * @param e US or METRIC
+	 * @return
+	 */
+	public int bmrCalculator(int height, int weight, int age, int gender, UnitType e) {
+		return 0;
+	}
+	/**
+	 * This will generate the body type of the user
+	 * @param bust
+	 * @param waist
+	 * @param hipHeight
+	 * @param hipSize
+	 * @param e US or METRIC
+	 * @return
+	 */
+	public String bodyTypeCalculator(int bust, int waist, int hipHeight, int hipSize, UnitType e) {
+		return null;
+	}
+	
+	/**
+	 * This will generate a healthy weight based off the users height at weight
+	 * @param height
+	 * @param weight
+	 * @param e
+	 * @return
+	 */
+	public int healthyWeightCalculator(int height, int weight, UnitType e) {
+		return 0;
+	}
+	
+	
+
+}
---
+++ FitnessCalculator/src/fitnessCalculator/Person.java
@@ -0,0 +1,177 @@
+package fitnessCalculator;
+
+public class Person {
+	private UnitType e;
+	private String firstName;
+	private String lastName;
+	private int age;
+	private int gender;
+	private int weightLbs;
+	private int weightKg;
+	private int heightFt;
+	private int heightInch;
+	private int heightCm; 
+	private int bustSize;
+	private int waistSize;
+	private int hipHeight;
+	private int hipSize;
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+	/**
+	 * @return the age
+	 */
+	public int getAge() {
+		return age;
+	}
+	/**
+	 * @return the gender
+	 */
+	public int getGender() {
+		return gender;
+	}
+	/**
+	 * @return the weightLbs
+	 */
+	public int getWeightLbs() {
+		return weightLbs;
+	}
+	/**
+	 * @return the weightKg
+	 */
+	public int getWeightKg() {
+		return weightKg;
+	}
+	/**
+	 * @return the heightFt
+	 */
+	public int getHeightFt() {
+		return heightFt;
+	}
+	/**
+	 * @return the heightInch
+	 */
+	public int getHeightInch() {
+		return heightInch;
+	}
+	/**
+	 * @return the heightCm
+	 */
+	public int getHeightCm() {
+		return heightCm;
+	}
+	/**
+	 * @return the bustSize
+	 */
+	public int getBustSize() {
+		return bustSize;
+	}
+	/**
+	 * @return the waistSize
+	 */
+	public int getWaistSize() {
+		return waistSize;
+	}
+	/**
+	 * @return the hipHeight
+	 */
+	public int getHipHeight() {
+		return hipHeight;
+	}
+	/**
+	 * @return the hipSize
+	 */
+	public int getHipSize() {
+		return hipSize;
+	}
+	/**
+	 * @param firstName the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	/**
+	 * @param lastName the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	/**
+	 * @param age the age to set
+	 */
+	public void setAge(int age) {
+		this.age = age;
+	}
+	/**
+	 * @param gender the gender to set
+	 */
+	public void setGender(int gender) {
+		this.gender = gender;
+	}
+	/**
+	 * @param weightLbs the weightLbs to set
+	 */
+	public void setWeightLbs(int weightLbs) {
+		this.weightLbs = weightLbs;
+	}
+	/**
+	 * @param weightKg the weightKg to set
+	 */
+	public void setWeightKg(int weightKg) {
+		this.weightKg = weightKg;
+	}
+	/**
+	 * @param heightFt the heightFt to set
+	 */
+	public void setHeightFt(int heightFt) {
+		this.heightFt = heightFt;
+	}
+	/**
+	 * @param heightInch the heightInch to set
+	 */
+	public void setHeightInch(int heightInch) {
+		this.heightInch = heightInch;
+	}
+	/**
+	 * @param heightCm the heightCm to set
+	 */
+	public void setHeightCm(int heightCm) {
+		this.heightCm = heightCm;
+	}
+	/**
+	 * @param bustSize the bustSize to set
+	 */
+	public void setBustSize(int bustSize) {
+		this.bustSize = bustSize;
+	}
+	/**
+	 * @param waistSize the waistSize to set
+	 */
+	public void setWaistSize(int waistSize) {
+		this.waistSize = waistSize;
+	}
+	/**
+	 * @param hipHeight the hipHeight to set
+	 */
+	public void setHipHeight(int hipHeight) {
+		this.hipHeight = hipHeight;
+	}
+	/**
+	 * @param hipSize the hipSize to set
+	 */
+	public void setHipSize(int hipSize) {
+		this.hipSize = hipSize;
+	}
+	
+	
+
+}
